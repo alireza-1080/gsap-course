477523e0de12d19dadf01a3116e1da31e7b2f526
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-19 13:22:06 -0700

    CSS Transition 3D P.2

--- app/css-transform-3d/page.tsx
+++ app/css-transform-3d/page.tsx
@@ -16,13 +16,31 @@ const CssTransform3d = () => {
         <h2>Hover: Rotate Y 45 deg</h2>
         <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-y-45">Normal div element</div>
       </div>
-      <div>
+      <div className='perspective-midrange'>
         <h2>Hover: Rotate X 45 deg</h2>
-        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-x-45">Normal div element</div>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 hover:rotate-x-45">Normal div element</div>
       </div>
-      <div>
+      <div className='perspective-midrange'>
         <h2>Hover: Rotate X Y Z 45 deg</h2>
-        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-x-45 hover:rotate-y-45 hover:rotate-z-45">
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 hover:rotate-x-45 hover:rotate-y-45 hover:rotate-z-45">
+          Normal div element
+        </div>
+      </div>
+      <div className='perspective-midrange'>
+        <h2>Hover: Translate X 288px</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 hover:translate-x-72">
+          Normal div element
+        </div>
+      </div>
+      <div className='perspective-midrange'>
+        <h2>Hover: Translate Y 96px</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 hover:translate-y-24">
+          Normal div element
+        </div>
+      </div>
+      <div className='perspective-midrange'>
+        <h2>Hover: Translate z 192px</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 hover:translate-z-48">
           Normal div element
         </div>
       </div>
