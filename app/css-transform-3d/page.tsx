f2511abd796b74b68a50fc8cbb559e43666b9f91
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-19 13:09:30 -0700

    CSS Transition 3D P.1

---
+++ app/css-transform-3d/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const CssTransform3d = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center text-5xl *:flex *:h-screen *:w-full *:flex-col *:items-center *:justify-center *:gap-10">
+      <h1 className="text-9xl">Css Transform 3D 😍</h1>
+      <div>
+        <h2>No transform</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10">Normal div element</div>
+      </div>
+      <div>
+        <h2>Hover: Rotate Z 45 deg</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-z-45">Normal div element</div>
+      </div>
+      <div>
+        <h2>Hover: Rotate Y 45 deg</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-y-45">Normal div element</div>
+      </div>
+      <div>
+        <h2>Hover: Rotate X 45 deg</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-x-45">Normal div element</div>
+      </div>
+      <div>
+        <h2>Hover: Rotate X Y Z 45 deg</h2>
+        <div className="h-fit w-fit border-4 border-blue-500 bg-gray-900 p-10 duration-500 perspective-near hover:rotate-x-45 hover:rotate-y-45 hover:rotate-z-45">
+          Normal div element
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CssTransform3d;
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className="w-full min-w-screen flex flex-col justify-center items-center gap-10 p-20 *:w-max">
-      <button className="px-6 py-3 bg-blue-500 rounded-lg">
-        <Link href="/css-filters">CSS Filters</Link>
-      </button>
-      <button className="px-6 py-3 bg-blue-500 rounded-lg">
-        <Link href="/css-transition">CSS Transition</Link>
-      </button>
-      <button className="px-6 py-3 bg-blue-500 rounded-lg">
-        <Link href="/css-transform-2d">Transform 2D</Link>
-      </button>
+    <div className="flex w-full min-w-screen flex-col items-center justify-center gap-10 p-20 *:w-max">
+      <Link href="/css-filters">
+        <button className="cursor-pointer rounded-lg bg-blue-500 px-6 py-3">CSS Filters</button>
+      </Link>
+      <Link href="/css-transition">
+        <button className="cursor-pointer rounded-lg bg-blue-500 px-6 py-3">CSS Transition</button>
+      </Link>
+      <Link href="/css-transform-2d">
+        <button className="cursor-pointer rounded-lg bg-blue-500 px-6 py-3">Transform 2D</button>
+      </Link>
+      <Link href="/css-transform-3d">
+        <button className="cursor-pointer rounded-lg bg-blue-500 px-6 py-3">Transform 3D</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
