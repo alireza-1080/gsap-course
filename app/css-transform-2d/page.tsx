4bec4ca154fe1ef436435817ef474d88466a0d6b
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-19 12:35:46 -0700

    CSS Transition 2D P.2

--- app/css-transform-2d/page.tsx
+++ app/css-transform-2d/page.tsx
@@ -16,6 +16,30 @@ const CssTransform2D = () => {
         <h2>Translate X -50%</h2>
         <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 -translate-x-1/2'>Normal div element</div>
       </div>
+      <div>
+        <h2>Scale X 150%</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 scale-x-150'>Normal div element</div>
+      </div>
+      <div>
+        <h2>Scale Y 150%</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 scale-y-150'>Normal div element</div>
+      </div>
+      <div>
+        <h2>Scale 150%</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 scale-150'>Normal div element</div>
+      </div>
+      <div>
+        <h2>Skew X 12 deg</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 skew-x-12'>Normal div element</div>
+      </div>
+      <div>
+        <h2>Skew Y 12 deg</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 skew-y-12'>Normal div element</div>
+      </div>
+      <div>
+        <h2>Skew 12 deg</h2>
+        <div className='w-fit h-fit p-10 border-4 border-blue-500 bg-gray-900 skew-12'>Normal div element</div>
+      </div>
     </div>
   );
 };
